28f833ac7a0e8b7d021299e13e1994907ce0f73b
yobosis <daksh.guzzar@gmail.com>
2025-06-04 14:11:47 +0530

    Update countingsort.c

--- countingsort.c
+++ countingsort.c
@@ -11,28 +11,28 @@ void countingSort(int arr[], int n) {
     int *count = (int *)malloc((k + 1) * sizeof(int));
     int *output = (int *)malloc(n * sizeof(int));
     
-    // Initialize count array
+
     for (int i = 0; i <= k; i++) {
         count[i] = 0;
     }
     
-    // Store the count of each element
+
     for (int i = 0; i < n; i++) {
         count[arr[i]]++;
     }
     
-    // Change count[i] so that it contains the actual position of this element in output[]
+
     for (int i = 1; i <= k; i++) {
         count[i] += count[i - 1];
     }
     
-    // Build the output array
+
     for (int i = n - 1; i >= 0; i--) {
         output[count[arr[i]] - 1] = arr[i];
         count[arr[i]]--;
     }
     
-    // Copy the output array to arr[], so that arr[] now contains sorted numbers
+
     for (int i = 0; i < n; i++) {
         arr[i] = output[i];
         printf("\t%d",arr[i]);
